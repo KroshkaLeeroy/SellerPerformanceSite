e27465f7168ff3fdb023732b8b1e38938a838229
KroshkaLeeroy <vasiliuvetrov@mail.ru>
2024-04-22 21:04:10 +0300

    Изменение структуры наследования

--- application/models.py
+++ application/models.py
@@ -1,7 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-from application.db_setup import db
-
+db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user_table'
@@ -12,36 +12,34 @@ class User(db.Model, UserMixin):
     is_account_active = db.Column(db.Boolean, default=True)
     account_type = db.Column(db.String(128), default='')
 
-    children = (db.relationship("Keys", back_populates="parent"), db.relationship("Requests", back_populates="parent"),
-                db.relationship("Payments", back_populates="parent"))
-
+    keys = db.relationship("Keys", back_populates="parent")
+    requests = db.relationship("Requests", back_populates="parent")
+    payments = db.relationship("Payments", back_populates="parent")
 
 class Keys(db.Model):
     __tablename__ = 'keys_table'
 
     parent_id = db.Column(db.Integer, db.ForeignKey("user_table.id"), primary_key=True)
-    parent = db.relationship("User", back_populates="children")
+    parent = db.relationship("User", back_populates="keys")
 
     api_key_seller = db.Column(db.String(255), default='')
     client_id_seller = db.Column(db.String(255), default='')
     api_key_performance = db.Column(db.String(255), default='')
     client_id_performance = db.Column(db.String(255), default='')
 
-
 class Requests(db.Model):
     __tablename__ = 'requests_table'
 
     parent_id = db.Column(db.Integer, db.ForeignKey("user_table.id"), primary_key=True)
-    parent = db.relationship("User", back_populates="children")
+    parent = db.relationship("User", back_populates="requests")
 
     request_count = db.Column(db.Integer, default=0)
-
 
 class Payments(db.Model):
     __tablename__ = 'payments_table'
 
     parent_id = db.Column(db.Integer, db.ForeignKey("user_table.id"), primary_key=True)
-    parent = db.relationship("User", back_populates="children")
+    parent = db.relationship("User", back_populates="payments")
 
     plan = db.Column(db.String(128), default='trial')
     is_plan_active = db.Column(db.Boolean, default=True)
