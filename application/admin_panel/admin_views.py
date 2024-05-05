54a0ac7d06b8accfe226849692bbb9cc704a4abf
KroshkaLeeroy <vasiliuvetrov@mail.ru>
2024-05-05 21:12:06 +0300

    Добавление возможности просматривать логи 0.06

--- application/admin_panel/admin_views.py
+++ application/admin_panel/admin_views.py
@@ -104,7 +104,7 @@ def report_log(user_id, report_data):
     print(data, url)
     if data.status_code == 200:
         try:
-            return render_template('admin_panel_report_log.html', data=data.json())
+            return render_template('admin_log_reports_reader.html', data=data.json())
         except Exception as e:
             return jsonify({'error': f'error {e}'}), 400
     return jsonify({'error': f'report log not found {data.text}'}), 400
