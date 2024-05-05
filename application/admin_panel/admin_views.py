702d0c415e0fa63f589b3bb439b9fc95fbe54c17
KroshkaLeeroy <vasiliuvetrov@mail.ru>
2024-05-05 20:42:04 +0300

    Добавление возможности просматривать логи 0.01

--- application/admin_panel/admin_views.py
+++ application/admin_panel/admin_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, request, url_for
+from flask import Blueprint, render_template, redirect, request, url_for, jsonify
 from flask_login import current_user, login_required
 from application.setup import User, db
 from application.config import URL_TO_API
@@ -92,6 +92,19 @@ def admin_page_user_delete(user_id, action):
         user.account_status = 'deleted'
         db.session.commit()
     return redirect(url_for('admin_blueprint.admin_page'))
+
+
+@admin_blueprint.route('/report_log/<user_id>/<report_data>', methods=['GET', 'POST'])
+@login_required
+def report_log(user_id, report_data):
+    if current_user.account_type != 'admin':
+        return redirect('/profile')
+    url = f'{URL_TO_API}/MraK1911_!@#/{user_id}/{report_data}'
+    data = requests.get(url)
+    print(data, url)
+    if data.status_code == 200:
+        return jsonify(data.text)
+    return jsonify({'error': f'report log not found {data.text}'}), 400
 
 
 @admin_blueprint.route('/admin_panel/<int:user_id>/edit', methods=['GET', 'POST'])
