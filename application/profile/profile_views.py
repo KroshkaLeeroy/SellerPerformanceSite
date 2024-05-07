c5cef2b6d6f049e332ee3eb180aa30382c5a3b91
KroshkaLeeroy <vasiliuvetrov@mail.ru>
2024-05-07 12:47:46 +0300

    Правки создания отчета

--- application/profile/profile_views.py
+++ application/profile/profile_views.py
@@ -21,22 +21,22 @@ def profile_page():
         api_performance = request.form.get('api_performance')
         client_id_performance = request.form.get('client_id_performance')
 
-        mistake = True
-        if len(api_seller) != 36:
-            flash('Seller Secret должен содержать 36 символов')
-            mistake = False
-        if len(api_performance) != 82:
-            flash('Performance API должен содержать 82 символа')
-            mistake = False
-        if not client_id_seller.isdigit():
-            flash('Client ID должен содержать только цифры')
-            mistake = False
-        if "@advertising.performance.ozon.ru" not in client_id_performance:
-            flash('Performance ID должен содержать @advertising.performance.ozon.ru')
-            mistake = False
-
-        if mistake:
-            return render_template('edit_profile.html', user=current_user, req_c=req, keys=keys)
+        # mistake = True
+        # if len(api_seller) != 36:
+        #     flash('Seller Secret должен содержать 36 символов')
+        #     mistake = False
+        # if len(api_performance) != 82:
+        #     flash('Performance API должен содержать 82 символа')
+        #     mistake = False
+        # if not client_id_seller.isdigit():
+        #     flash('Client ID должен содержать только цифры')
+        #     mistake = False
+        # if "@advertising.performance.ozon.ru" not in client_id_performance:
+        #     flash('Performance ID должен содержать @advertising.performance.ozon.ru')
+        #     mistake = False
+        #
+        # if mistake:
+        #     return render_template('edit_profile.html', user=current_user, req_c=req, keys=keys)
 
         current_user.email = request.form.get('email')
         keys.api_key_seller = api_seller
@@ -76,38 +76,41 @@ def query_page():
         if not date_to or not date_from:
             flash('Даты должны быть указаны')
         else:
-            try:
-                # Конвертация дат из строк в объекты datetime
-                date_from_obj = datetime.strptime(date_from, "%d.%m.%Y")
-                date_to_obj = datetime.strptime(date_to, "%d.%m.%Y")
-                current_date = datetime.now()
+            print(date_from, date_to)
+            # Конвертация дат из строк в объекты datetime
+            date_from_obj = datetime.strptime(date_from, "%Y-%m-%d")
+            date_to_obj = datetime.strptime(date_to, "%Y-%m-%d")
+            current_date = datetime.now()
 
-                # Проверки на порядок дат и на диапазон дат
-                if date_from_obj >= date_to_obj:
-                    flash('"Дата с" должна быть раньше "Дата по"')
-                    return redirect(url_for('profile_blueprint.query_page'))
+            # Проверки на порядок дат и на диапазон дат
+            if date_from_obj > date_to_obj:
+                flash('"Дата с" должна быть раньше "Дата по"')
+                return redirect(url_for('profile_blueprint.query_page'))
 
-                days_diff = date_to_obj - date_from_obj
-                if days_diff.days > 32:
-                    flash('Слишком большая разница между датами, максимальный диапазон 32 дня')
-                    return redirect(url_for('profile_blueprint.query_page'))
+            days_diff = date_to_obj - date_from_obj
+            if days_diff.days > 32:
+                flash('Слишком большая разница между датами, максимальный диапазон 32 дня')
+                return redirect(url_for('profile_blueprint.query_page'))
 
-                # Специфичные проверки на date_to
-                yesterday = current_date - timedelta(days=1)
-                thirty_two_days_ago = current_date - timedelta(days=32)
-                if not (yesterday.date() <= date_to_obj.date() <= thirty_two_days_ago.date()):
-                    flash('date_to должен быть вчерашним днем и не ранее 32-х дней от текущей даты')
-                    return redirect(url_for('profile_blueprint.query_page'))
+            # Специфичные проверки на date_to
+            yesterday = current_date - timedelta(days=1)
+            thirty_two_days_ago = current_date - timedelta(days=32)
+            if date_from_obj > yesterday:
+                flash('"Дата с" должна быть минимум вчерашним днем')
+                return redirect(url_for('profile_blueprint.query_page'))
 
-                # Отправка данных на сервер
-                response = requests.post(f'{URL_TO_API}/add-request', json=data)
-                if response.status_code == 200:
-                    flash('Запрос успешно отправлен на сервер')
-                else:
-                    flash('Нет возможности соединиться с сервером запросов')
+            if date_from_obj < thirty_two_days_ago:
+                flash('"Дата с" не должна быть больше 32 дней назад')
+                return redirect(url_for('profile_blueprint.query_page'))
 
-            except ValueError:
-                flash('Ошибка в формате даты: ожидается формат DD.MM.YYYY')
+            # Отправка данных на сервер
+            response = requests.post(f'{URL_TO_API}/add-request', json=data)
+            if response.status_code == 200:
+                flash('Запрос успешно отправлен на сервер')
+            else:
+                flash('Нет возможности соединиться с сервером запросов')
+
+
 
         return redirect(url_for('profile_blueprint.query_page'))
     try:
@@ -122,14 +125,15 @@ def query_page():
     if keys.api_key_seller == '' or keys.api_key_performance == '' or keys.client_id_seller == '' or keys.client_id_performance == '':
         flash(f'Заполните API ключи в Настройках профиля')
 
-    if len(keys.api_key_seller) != 36:
-        flash('Seller Secret должен содержать 36 символов')
-    if len(keys.api_key_performance) != 82:
-        flash('Performance API должен содержать 82 символа')
-    if not keys.client_id_seller.isdigit():
-        flash('Client ID должен содержать только цифры')
-    if "@advertising.performance.ozon.ru" not in keys.client_id_performance:
-        flash('Performance ID должен содержать @advertising.performance.ozon.ru')
+    # if len(keys.api_key_seller) != 36:
+    #     flash('Seller Secret должен содержать 36 символов')
+    # if len(keys.api_key_performance) != 82:
+    #     print(keys.api_key_performance)
+    #     flash('Performance API должен содержать 82 символа')
+    # if not keys.client_id_seller.isdigit():
+    #     flash('Client ID должен содержать только цифры')
+    # if "@advertising.performance.ozon.ru" not in keys.client_id_performance:
+    #     flash('Performance ID должен содержать @advertising.performance.ozon.ru')
 
     return render_template('query.html', reports=reports, user=current_user, URL=URL_TO_API)
 
