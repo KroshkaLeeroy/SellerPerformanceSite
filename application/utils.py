f80665467c9aa27fee18c1483196cdd1a422eee2
KroshkaLeeroy <vasiliuvetrov@mail.ru>
2024-06-21 19:29:22 +0300

    Изменение формата вывода дат в админке 0.3.0

--- application/utils.py
+++ application/utils.py
@@ -60,7 +60,8 @@ def check_reports_from_API(url, user_id):
 
 
 def check_reports_from_API_dev_log(url_to_api, admin_key, user_id, report_data):
-    time_from, time_to = report_data.split('_')
+    date_info = report_data.split('_')
+    time_from, time_to = date_info[0], date_info[1]
     time_from, time_to = time_from.split('.')[::-1], time_to.split('.')[::-1]
     time_from, time_to = '-'.join(time_from), '-'.join(time_to)
     report_data = f'{time_from}_{time_to}'
